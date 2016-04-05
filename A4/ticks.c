1c6fb29a7b8a0ff3373fdb54108199422831ef78
jauthier <jauthier@potter.socs.uoguelph.ca>
2016-04-05 12:49:25 -0400

    send

--- A4/ticks.c
+++ A4/ticks.c
@@ -2,22 +2,30 @@
 #include "stdio.h"
 #include "stdlib.h"
 #define LONG 2814585000
-int main () {
+int main (int argc, char *argv[]) {
 
    int i;
    ticks t1;
    ticks t2;
    double mytime1;
    double mytime2;
+   char buffer[5000];
+   FILE *fp;
 
-   for ( i=0; i<10; i++ ) {
-      t1 = getticks();
-      sleep(1);
-      t2 = getticks();
-      mytime1 = elapsed(t2,t1);
-      mytime2 = elapsed(t2,t1) / LONG;
-      printf ("%f	%f\n",mytime1,mytime2);
+   if (argc == 1){
+      printf("Please enter the name of the file\n");
+      exit(0);
    }
+   char *fileName = argv[1];
+   fp = fopen(fileName, "r");
+   t1 = getticks();
+
+   fread(buffer, 5000, 1, fp);
+
+   t2 = getticks();
+   mytime1 = elapsed(t2,t1);
+   mytime2 = elapsed(t2,t1) / LONG;
+   printf ("%f	%f\n",mytime1,mytime2);
 
 }
 
